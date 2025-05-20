013d59e9006fe77cada91bb9600b5244e1664554
MuratYucedag <yucedagmurat23@gmail.com>
2025-05-20 22:03:03 +0300

    ...

--- ShoppingNightMongo/Services/CategoryServices/CategoryService.cs
+++ ShoppingNightMongo/Services/CategoryServices/CategoryService.cs
@@ -26,18 +26,20 @@ namespace ShoppingNightMongo.Services.CategoryServices
         {
             await _categoryCollection.DeleteOneAsync(id);
         }
-        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
         {
-            throw new NotImplementedException();
+            var values = await _categoryCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultCategoryDto>>(values);
         }
-        public Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id)
+        public async Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id)
         {
-            throw new NotImplementedException();
+            var value = await _categoryCollection.Find(x => x.CategoryId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetCategoryByIdDto>(value);
         }
         public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
         {
             var value = _mapper.Map<Category>(updateCategoryDto);
-            await _categoryCollection.FindOneAndReplaceAsync(x=>x.CategoryId==updateCategoryDto.CategoryId, value);
+            await _categoryCollection.FindOneAndReplaceAsync(x => x.CategoryId == updateCategoryDto.CategoryId, value);
         }
     }
 }
